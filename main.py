88365820191a58762e703dc15c2cb691b6aa3506
Joseph Dale <jdale@dnanexus.com>
2012-05-02 13:34:20 -0700

    Initial commit, copying from cells repo.

---
+++ main.py
@@ -0,0 +1,14 @@
+import dxpy
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Program to download the contents of a URL and upload them into the
+# platform as a file object.
+
+def main():
+    url = job['input']['url']
+    subprocess.check_call(["curl", url, "-o", "fetched_from_url"])
+    new_file = dxpy.upload_local_file("fetched_from_url")
+    job['output'] = {'file': {'$dnanexus_link': new_file.get_id()}}
