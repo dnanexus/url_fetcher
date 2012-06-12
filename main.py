665574c5f9780ceb40c70d5a6be8d2cdd024b74e
Joseph Dale <jdale@dnanexus.com>
2012-06-12 15:22:49 -0700

    Manually install curl from within the app, rather than in execDepends. Workaround for bug in worker AMI.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ logging.basicConfig(level=logging.DEBUG)
 # platform as a file object.
 
 def main():
+    subprocess.check_call("sudo apt-get install curl --yes --force-yes", shell=True)
     url = job['input']['url']
     subprocess.check_call(["curl", url, "-o", "fetched_from_url"])
     new_file = dxpy.upload_local_file("fetched_from_url")
