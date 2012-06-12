6ad116b24160437f4454c16fb352db0105a5d6a1
Joseph Dale <jdale@dnanexus.com>
2012-06-12 16:12:38 -0700

    Extract filename from URL.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import dxpy
 import subprocess
 import logging
+import urlparse
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -8,8 +10,13 @@ logging.basicConfig(level=logging.DEBUG)
 # platform as a file object.
 
 def main():
-    subprocess.check_call("sudo apt-get install curl --yes --force-yes", shell=True)
+    subprocess.check_call("sudo apt-get install curl --yes --force-yes", shell = True)
     url = job['input']['url']
+    url_path = urlparse.urlparse(url).path
+    file_name = os.path.basename(url_path)
+    if file_name == '':
+        file_name = url_path
     subprocess.check_call(["curl", "-k", url, "-o", "fetched_from_url"])
     new_file = dxpy.upload_local_file("fetched_from_url")
+    new_file.rename(file_name)
     job['output'] = {'file': {'$dnanexus_link': new_file.get_id()}}
