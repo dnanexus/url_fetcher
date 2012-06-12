cbea443ca305076077c99987ca5d1ef2ea795f97
Joseph Dale <jdale@dnanexus.com>
2012-06-12 15:31:58 -0700

    Add -k argument to curl, to avoid certificate checking failures on the worker.

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ logging.basicConfig(level=logging.DEBUG)
 def main():
     subprocess.check_call("sudo apt-get install curl --yes --force-yes", shell=True)
     url = job['input']['url']
-    subprocess.check_call(["curl", url, "-o", "fetched_from_url"])
+    subprocess.check_call(["curl", "-k", url, "-o", "fetched_from_url"])
     new_file = dxpy.upload_local_file("fetched_from_url")
     job['output'] = {'file': {'$dnanexus_link': new_file.get_id()}}
