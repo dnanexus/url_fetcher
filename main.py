b5bf47533e2f9b188ff85118169330075b2074fb
George Asimenos <george+github@dnanexus.com>
2012-07-02 14:55:42 -0700

    + Blocking on file

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def main():
     subprocess.check_call(["aria2c", url, "-o", "fetched_from_url", "-x6", "-s6", "-j6", "--check-certificate=false"])
     file_folder = dxpy.DXJob(job["id"]).describe()['folder']
     print "Uploading file into project " + dxpy.PROJECT_CONTEXT_ID + " (from dxpy.PROJECT_CONTEXT_ID)"
-    new_file = dxpy.upload_local_file("fetched_from_url", project = dxpy.PROJECT_CONTEXT_ID, name = file_name, folder = file_folder, parents = True)
+    new_file = dxpy.upload_local_file("fetched_from_url", project = dxpy.PROJECT_CONTEXT_ID, name = file_name, folder = file_folder, parents = True, wait_on_close = True)
     job['output'] = {'file': {'$dnanexus_link': new_file.get_id()}}
