8ee3f20dcfadbcdf967d461b6b0558487be1684a
andrew carroll <acarroll@dnanexus.com>
2013-01-14 17:32:58 -0800

    Switch to new python interpreter

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ logging.basicConfig(level=logging.DEBUG)
 # Program to download the contents of a URL and upload them into the
 # platform as a file object.
 
-def main():
-    url = job['input']['url']
+@dxpy.entry_point('main')
+def main(**job_input):
+    url = job_input['url']
     url_path = urlparse.urlparse(url).path
     file_name = os.path.basename(url_path)
     if file_name == '':
         file_name = url_path
     if file_name == '':
         file_name = url
-    file_folder = dxpy.DXJob(job["id"]).describe()['folder']
+    file_folder = dxpy.DXJob(dxpy.JOB_ID).describe()['folder']
     placeholder_dxfile = dxpy.new_dxfile(name = file_name,
                                          folder = file_folder,
                                          parents = True,
@@ -28,13 +29,12 @@ def main():
         new_file = dxpy.upload_local_file("fetched_from_url",
                                           keep_open=True,
                                           use_existing_dxfile = placeholder_dxfile)
-        if 'additional_types' in job['input']:
-            new_file.add_types([x.strip() for x in job['input']['additional_types'].split(",")])
-        if 'output_name' in job['input']:
-            if job['input']['output_name'] != '':
-                new_file.rename(job['input']['output_name'])
+        if 'additional_types' in job_input:
+            new_file.add_types([x.strip() for x in job_input['additional_types'].split(",")])
+        if job_input.get('output_name') != None and job_input.get('output_name') != '':
+            new_file.rename(job_input['output_name'])
         new_file.close(block=True)
     except:
         placeholder_dxfile.remove()
         raise
-    job['output'] = {'file': {'$dnanexus_link': new_file.get_id()}}
+    return {'file': {'$dnanexus_link': new_file.get_id()}}
